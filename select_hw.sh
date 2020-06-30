301a7f3c439fbe2068cfc7c5bd0435641f7e883d
Cloud User <centos@ip-172-31-80-204.ec2.internal>
2020-06-30 17:31:20 +0000

    initial

---
+++ select_hw.sh
@@ -0,0 +1,3 @@
+rm kmer.fpga.xclbin
+#ln -s Hardware/binary_container_1.xclbin kmer.fpga.xclbin
+ln -s AFI/binary_container_1.awsxclbin kmer.fpga.xclbin
---
+++ select_sw_emu.sh
@@ -0,0 +1,2 @@
+rm kmer.fpga.xclbin
+ln -s Emulation-SW/binary_container_1.xclbin kmer.fpga.xclbin
